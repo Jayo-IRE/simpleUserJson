b9560c7c697e8e5a5004b7a3c69565379e713fec
Jayo-IRE <jasonfinnegan@gmail.com>
2016-08-02 18:18:53 +0100

    Add files via upload

---
+++ Test_part2/Question2.js
@@ -0,0 +1,40 @@
+var one;
+	var two;
+function remoteMathService(cb){
+	
+	callOneService(function(err,num){
+		one=num;
+		console.log("one=num",num);
+//		cb(undefined,one +two);
+	});
+	callTwoService(function(err,num){
+		two=num;
+		console.log("two=num",num);
+		cb(undefined,one +two);
+	});
+
+	//return cb(undefined,one + two);
+}
+
+function callOneService(cb){
+	setTimeout(function(){
+		return cb(undefined,1);
+	},1000);
+}
+function callTwoService(cb){
+	setTimeout(function(){
+		return cb(undefined,2);
+	},1500);
+}
+
+remoteMathService(function(err,answer){
+	console.log("answer= ", one+two );
+	
+	if(err)console.log("error",err);
+	if(answer!==3){
+		console.log("wronganswer",answer);
+	}else{
+	console.log("correct");
+	}
+	
+});
---
+++ Test_part2/Question3.js
@@ -0,0 +1,47 @@
+// This is a mock database implementation with just a connect function
+// db.connect will need to be called a total of 10 times before it successfully connects
+var counter = 0;
+var db = {
+	connect: function(cb) {
+		console.log('connection attempt', counter + 1)
+		if (counter < 9) {
+			counter++;
+			return cb('db not ready yet');
+		}
+	return cb();
+	}
+}
+// Try to connect, log a successful connection & exit
+// If we fail to connect, log an error and return
+
+/*
+db.connect(function(err) {
+		console.log('count=',counter);
+		if (err) {
+			console.error('error here', err);
+			attempts++;
+			return;
+		}	
+		console.log('successfully connected!');
+
+});
+*/
+
+
+function calldbBackoff(err,delay){
+	console.log('delay=',delay);
+	setTimeout(function(){
+		db.connect(function(err) {
+		if (err) {
+			console.error('error here', err);
+			calldbBackoff(null,delay*2);
+			}
+		else{
+			console.log('successfully connected!');
+			}
+		});
+	}, delay);
+};
+
+
+calldbBackoff(null,5);
