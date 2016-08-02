e72268effa4bc82e8770961b66619c3d4d63fc21
Jayo-IRE <jasonfinnegan@gmail.com>
2016-08-02 18:56:15 +0100

    Update Question1.js
    added return after if(err) on line 3

--- Test_part2/Question1.js
+++ Test_part2/Question1.js
@@ -1,14 +1,10 @@
-function doThing(){
-	console.log('doing the thing');
-	//throw new Error('something bad happened');
+function foo (callback){
+	doThing(function (err,res){
+		if(err) return callback(err);	
+		callback(   null   ,res);
+	});
 }
 
-function foo (callback){
-doThing( function (err,res){
-  if   (err) callback(err);
-callback(   null   ,res);
+foo(function(err,res){
+	console.log ('Done!.err=',err,' :res= ',res);
 });
-}
-foo(   function   (err,res){
-  console   .log   (   'Done!.err=',err,' :res= ',res   );
-  });
