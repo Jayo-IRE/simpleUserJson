8011238e04f3c132463033b5e8498024b42eb9ab
Jayo-IRE <jasonfinnegan@gmail.com>
2016-08-02 19:08:09 +0100

    Update Question1.js

--- Test_part2/Question1.js
+++ Test_part2/Question1.js
@@ -1,7 +1,13 @@
+function doThing(callback){
+	console.log('doing the thing');
+	 callback("error", null);
+	//callback(null,"Something");
+}
+
 function foo (callback){
 	doThing(function (err,res){
 		if(err) return callback(err);	
-		callback(   null   ,res);
+		callback(null,res);
 	});
 }
 
