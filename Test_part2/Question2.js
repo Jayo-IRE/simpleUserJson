f23653fdaac07a96cb7cedafc84ce44a1b036c71
Jayo-IRE <jasonfinnegan@gmail.com>
2016-08-03 11:33:56 +0100

    Update Question2.js

--- Test_part2/Question2.js
+++ Test_part2/Question2.js
@@ -4,12 +4,9 @@ function remoteMathService(cb){
 	
 	callOneService(function(err,num){
 		one=num;
-		console.log("one=num",num);
-//		cb(undefined,one +two);
 	});
 	callTwoService(function(err,num){
 		two=num;
-		console.log("two=num",num);
 		cb(undefined,one +two);
 	});
 
@@ -28,8 +25,6 @@ function callTwoService(cb){
 }
 
 remoteMathService(function(err,answer){
-	console.log("answer= ", one+two );
-	
 	if(err)console.log("error",err);
 	if(answer!==3){
 		console.log("wronganswer",answer);
